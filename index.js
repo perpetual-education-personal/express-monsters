a075a1a87a9186b4896089d280b7d6a7386394f8
sheriffderek <derek@sheriffderek.consulting>
2022-12-11 08:01:07 -0800

    Add views and CSS

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 import express from 'express';
+import monsterData from './monsters.json' assert { type: 'json' };
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
+app.use( express.static('public') );
+app.use( express.static('css') );
+
+
+// Page routes
 app.get('/', function(request, response) {
-	console.log("Home page");
-	response.send("<h1>Home page</h1>")
+	response.render('home');
 });
+
+app.get('/monsters', function(request, response) {
+	response.render('monsters', { monsters: monsterData });
+});
+
+app.get('/monster', function(request, response) {
+	response.render('detail');
+});
+
+
+// If no route matches...
+app.use( function(request, response) {
+	response.status(404).render('404', { query: request.url });
+});
+
 
 app.listen(1982, function() {
 	console.log("Server started at http://localhost:1982");
