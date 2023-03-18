cc6364b8c2f35ea18383027a4f47bc0eba5c8594
sheriffderek <derek@sheriffderek.consulting>
2023-03-17 23:02:58 -0700

    Add contenful +
    - seems to be broken. Haven’t looked at this in a long time.

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 import express from 'express';
 import monsterData from './monsters.json' assert { type: 'json' };
 
+
+import contentful from 'contentful';
+
+const client = contentful.createClient({
+  space: '8gjcuad3dr24',
+  environment: 'master', // defaults to 'master' if not set
+  accessToken: 'qo2CFiuKeIkYyUD6lUM0302dAeo2EoG_MayfVfOCySo'
+})
+
+
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -13,11 +24,39 @@ app.use( express.static('css') );
 
 // Page routes
 app.get('/', function(request, response) {
-	response.render('home');
+
+	client.getEntry('3jblgGPeOqxPhG4vVv6XbR')
+		.then( function(entry) {
+			response.render('home', { page: entry.fields });
+		})
+		.catch(console.error)
+	;
 });
 
 app.get('/monsters', function(request, response) {
-	response.render('monsters', { monsters: monsterData });
+
+	client.getEntries({
+	  content_type: 'monster'
+	})
+	.then( function(data) {
+
+		const monsterData = data.items.map( function(item) {
+			return {
+				name: item.fields.name,
+				story: item.fields.story.content.content[0].value,
+				portrait: item.fields.portrait.fields.file.url,
+				adopted: item.fields.adopted,
+				birthday: item.fields.birthday,
+				color: item.fields.color,
+			};
+		});
+
+		console.log(monsterData);
+
+		response.render('monsters', { monsters: monsterData });
+	})
+	.catch(console.error);
+	
 });
 
 app.get('/monsters/:slug', function(request, response) {
